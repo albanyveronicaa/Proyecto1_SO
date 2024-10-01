6cabf2c6c03612341b0bebd037fbf70f1fa64968
Albany Avila <avila.albany@correo.unimet.edu.ve>
2024-10-01 19:44:52 -0400

    inicial

---
+++ src/main/java/com/mycompany/proyecto1_so/Proyecto1_SO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto1_so;
+
+/**
+ *
+ * @author Laptop
+ */
+public class Proyecto1_SO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
